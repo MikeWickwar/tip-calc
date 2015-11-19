0765a6389adf7e7736cb06c5e7163f4a3c013afa
Mike Wickwar <mike.wickwar@gmail.com>
2015-11-19 15:01:36 -0700

    initial tip calculator

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-window.alert("javascript is rockin");
+//test
+  alert("javascript is rockin");
+
+var btn = document.getElementById('submit');
+
+btn.addEventListener('click', function(){
+// console.log('test1')
+var tA = document.getElementById('tA')
+var money = tA.value;
+
+
+console.log(money)
+var tip = (money * .2);
+// console.log('test2')
+var output = document.getElementById('output')
+// console.console.log('test3');
+output.innerHTML = "You should tip:" +tip.toFixed(2);
+// console.log('test4');
+
+})
